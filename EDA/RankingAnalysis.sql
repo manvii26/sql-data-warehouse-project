afa5443280287c0d7b782b943bbe06d500356d93
Manswi Sati <manvisati03@gmail.com>
2026-01-19 14:53:53 +0530

    Add Ranking Analysis SQL queries
    Added SQL queries for ranking analysis of products and customers, including top and bottom performers based on revenue.

---
+++ EDA/RankingAnalysis.sql
@@ -0,0 +1,79 @@
+-- Ranking Analysis
+
+-- order the values of dimensions by measure
+-- Top N performers | Bottom N performers
+
+
+-- 5 products generate the highest revenue
+SELECT TOP 5 
+p.product_name,
+SUM(f.sales_amount) AS revenue_by_product
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY revenue_by_product DESC
+
+-- with window function
+
+SELECT *
+FROM (
+SELECT  
+p.product_name,
+SUM(f.sales_amount) AS revenue_by_product,
+ROW_NUMBER() OVER (ORDER BY SUM(f.sales_amount) DESC ) AS rank_products
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name)t
+WHERE rank_products <=5
+
+
+
+
+-- 5 worst- performing products in terms of sales
+
+SELECT TOP 5 
+p.product_name,
+SUM(f.sales_amount) AS revenue_by_product
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY revenue_by_product ASC
+
+
+-- top 10 customers who have generated the highest revenue
+
+
+SELECT TOP 10
+	c.customer_key,
+	c.first_name,
+	c.last_name,
+	SUM(f.sales_amount) AS total_revenue	
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY
+	c.customer_key,
+	c.first_name,
+	c.last_name
+ORDER BY total_revenue DESC
+
+
+-- 3 customers with the fewest orders placed 
+
+SELECT TOP 3
+	c.customer_key,
+	c.first_name,
+	c.last_name,
+	COUNT(f.order_number) AS total_orders	
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY
+	c.customer_key,
+	c.first_name,
+	c.last_name
+ORDER BY total_orders 
+
